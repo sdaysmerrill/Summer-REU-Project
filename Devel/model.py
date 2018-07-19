d6030d18aa88742d08d3ebd5da722a910f7f6b09
Sarah Days-Merrill <sdaysmerrill@aol.com>
2018-07-19 12:52:14 -0500

    initial model

--- Devel/model.py
+++ Devel/model.py
@@ -16,60 +16,27 @@ MAX_LENGTH = 10
 USE_CUDA = False
 dropout_p = 0.05
 
-#encoder is still not quite right
-#still need to incorporate the slot value pairs with embedding
 
-class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size,dfs, n_layers):
-        super(EncoderRNN, self).__init__()
-        self.input_size = input_size    #748
-        self.hidden_size = hidden_size  #80
-        self.dfs = dfs                  #[0, 15, 149, 191, 200] - dimension of feature size
 
-        self.da = self.dfs[1] - self.dfs[0]   #15
-        self.ds = self.dfs[3] - self.dfs[2]   #42
-        self.dv = self.dfs[4] - self.dfs[3]   #9
 
-        #define random weighted matrices
-        self.Wah = 0.3 * torch.randn(self.da+1, self.hidden_size, dtype = torch.float64)
-        self.Wsh = 0.3 * torch.randn(self.ds+1, self.hidden_size, dtype = torch.float64)
-        self.Wvh = 0.3 * torch.randn(self.dv+1, self.hidden_size, dtype = torch.float64)
-        self.Wemb = 0.3 * torch.randn(self.input_size, self.hidden_size, dtype = torch.float64)
+def setWordVec(NetModel, word2vec):   
+    Wemb_np = NetModel.Wemb.get_value()
+    for w,v in word2vec.iteritems():
+        Wemb_np[w,:] = v
+    NetModel.Wemb.set_value(Wemb_np)
 
-        self.Wah[self.da,:] = 0.0
-        self.Wsh[self.ds,:] = 0.0
-        self.Wvh[self.dv,:] = 0.0
+def _emb(NetModel, a, s, v):
+    a_emb = torch.sum(NetModel.Wah[a,:],dim=0)  #sum over the rows
+    s_emb = NetModel.Wsh[s,:]
+    v_emb = NetModel.Wvh[v,:]
+    sv_emb= s_emb + v_emb
+    return a_emb, sv_emb
 
-        #set boundaries for the weighted matrices
-        self.Wah = torch.clamp(self.Wah, min = -1.0, max = 1.0)
-        self.Wsh = torch.clamp(self.Wsh, min = -1.0, max = 1.0)
-        self.Wvh = torch.clamp(self.Wvh, min = -1.0, max = 1.0)
-        self.Wemb = torch.clamp(self.Wemb, min = -1.0, max = 1.0) 
-
-        #specify emodel's parameters for optimization
-        self.Wah_parameter = nn.Parameter(self.Wah)
-        self.Wsh_parameter = nn.Parameter(self.Wsh)
-        self.Wvh_parameter = nn.Parameter(self.Wvh)
-        self.Wemb_parameter = nn.Parameter(self.Wemb)
-
-    def setWordVec(self, word2vec):   
-        self.Wemb_np = self.Wemb.get_value()
-        for w,v in word2vec.iteritems():
-            self.Wemb_np[w,:] = v
-        self.Wemb.set_value(self.Wemb_np)
-
-    def _emb(self, a, s, v):
-        a_emb = torch.sum(self.Wah[a,:],dim=0)  #sum over the rows
-        s_emb = self.Wsh[s,:]
-        v_emb = self.Wvh[v,:]
-        sv_emb= s_emb + v_emb
-        return a_emb, sv_emb
-
-    def unroll(self,a,s,v,words,cutoff_f,cutoff_b):
+def embedding(NetModel, a,s,v,words):
         
-        # embed DA
-        for i in range(10):  #don't know what range value should be
-            a_emb,sv_emb= self._emb(a,s,v)
+    # embed DA
+    a_emb,sv_emb= _emb(NetModel, a,s,v)
+    words_emb = setWordVec(NetModel, words)
  #       sv_emb = sv_emb.dimshuffle(1,0,2)
         # recurrence
  #       [h,c,p],_ = theano.scan(fn=self._recur,
@@ -79,9 +46,9 @@ class EncoderRNN(nn.Module):
         # compute desired sent_logp by slicing
 #        cutoff_logp = collectSentLogp(p,cutoff_f[4],cutoff_b)
 #        cost = -T.sum(cutoff_logp)
-        print("(debug) this is what a_emb holds ",a_emb)
-        print("(debug) this is what s_emb holds ",sv_emb)
-        return a_emb, sv_emb
+    print("(debug) this is what a_emb holds ",a_emb)
+    print("(debug) this is what s_emb holds ",sv_emb)
+    return a_emb, sv_emb ,words_emb
 
  #   def forward(self, tensor_a, tensor_s, tensor_v):
         # Note: we run this all at once (over the whole input sequence)
@@ -103,50 +70,72 @@ class EncoderRNN(nn.Module):
     
  
 #still need to incorporate this into the model in the decoder
-class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, embed_size):  
-        super(LSTM, self).__init__()
+class WenLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, Wgate, Who):  
+        super(WenLSTM, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
+        self.Wgate = Wgate
+        self.Who = Who
+
+      
+        
         # input embedding
  #       self.encoder = EncoderRNN(self.input_size, self.hidden_size) 
         # lstm weights
-        self.weight_fm = nn.Linear(hidden_size, hidden_size)
-        self.weight_im = nn.Linear(hidden_size, hidden_size)
-        self.weight_cm = nn.Linear(hidden_size, hidden_size)
-        self.weight_om = nn.Linear(hidden_size, hidden_size)
-        self.weight_fx = nn.Linear(embed_size, hidden_size)
-        self.weight_ix = nn.Linear(embed_size, hidden_size)
-        self.weight_cx = nn.Linear(embed_size, hidden_size)
-        self.weight_ox = nn.Linear(embed_size, hidden_size)
+   #     self.weight_fm = nn.Linear(hidden_size, hidden_size)
+#        self.weight_im = nn.Linear(hidden_size, hidden_size)
+#        self.weight_cm = nn.Linear(hidden_size, hidden_size)
+#        self.weight_om = nn.Linear(hidden_size, hidden_size)
+#        self.weight_fx = nn.Linear(embed_size, hidden_size)
+#        self.weight_ix = nn.Linear(embed_size, hidden_size)
+#        self.weight_cx = nn.Linear(embed_size, hidden_size)
+#        self.weight_ox = nn.Linear(embed_size, hidden_size)
         # multiplicative weights
-        self.weight_mh = nn.Linear(hidden_size, hidden_size)
-        self.weight_mx = nn.Linear(embed_size, hidden_size)
+#        self.weight_mh = nn.Linear(hidden_size, hidden_size)
+#        self.weight_mx = nn.Linear(embed_size, hidden_size)
         # decoder
  #       self.decoder = DecoderRNN(self.input_size, self.hidden_size, n_layers = 2, dropout_p = dropout_p)
 
 
-    def forward(self, inp, h_0, c_0):
+    def forward(self, wv_t, h_tm1, da_emb_t, h_0, c_0):
+        
+        gates_t = torch.dot(torch.cat((wv_t, h_tm1, da_emb_t), 1), self.Wgate)  #concatenate along axis = 1
+        #compute gates
+        ig = nn.Sigmoid(gates_t[:,:self.hidden_size])
+        fg = nn.Sigmoid(gates_t[:, self.hidden_size:self.hidden_size * 2])
+        og = nn.Sigmoid(gates_t[:,self.hidden_size * 2: self.hidden_size * 3])
+        cx_t = nn.Tanh(gates_t[:,self.hidden_size * 3:])
+        #update lstm internal state
+        c_t = ig * cx_t + fg * c_tm1
+        #obtain new hidden layer
+        h_t = og * nn.Tanh(c_t)
+        #compute output distribution target word probability
+        o_t = nn.Softmax(torch.dot(h_t, self.Who))
+        p_t = o_t[torch.arange(NetModel.db), y_t]  #where does db come from??
+
+        return h_t, c_t, p_t
+        
         # encode the input characters
-        inp = self.encoder(inp)
+   #     inp = self.encoder(inp)
         # calculate the multiplicative matrix
-        m_t = self.weight_mx(inp) * self.weight_mh(h_0)
+ #       m_t = self.weight_mx(inp) * self.weight_mh(h_0)
         # forget gate
-        f_g = F.sigmoid(self.weight_fx(inp) + self.weight_fm(m_t))
+#        f_g = F.sigmoid(self.weight_fx(inp) + self.weight_fm(m_t))
         # input gate
-        i_g = F.sigmoid(self.weight_ix(inp) + self.weight_im(m_t))
+#        i_g = F.sigmoid(self.weight_ix(inp) + self.weight_im(m_t))
         # output gate
-        o_g = F.sigmoid(self.weight_ox(inp) + self.weight_om(m_t))
+#        o_g = F.sigmoid(self.weight_ox(inp) + self.weight_om(m_t))
         # intermediate cell state
-        c_tilda = F.tanh(self.weight_cx(inp) + self.weight_cm(m_t))
+#        c_tilda = F.tanh(self.weight_cx(inp) + self.weight_cm(m_t))
         # current cell state
-        cx = f_g * c_0 + i_g * c_tilda
+#        cx = f_g * c_0 + i_g * c_tilda
         # hidden state
-        hx = o_g * F.tanh(cx)
+#        hx = o_g * F.tanh(cx)
 
-        out = self.decoder(hx.view(1,-1))
+#        out = self.decoder(hx.view(1,-1))
 
-        return out, hx, cx
+#        return out, hx, cx
 
     def init_hidden(self):   #do I need another init_hidden?? - well I need to initialize the context vector
         h_0 = Variable(torch.zeros(1, self.hidden_size))
@@ -181,9 +170,9 @@ class WenAttn(nn.Module):
         return F.softmax(attn_scores).unsqueeze(0).unsqueeze(0)
 
 
-    def attend(self,hidden_size, sv_emb):
-        score_x = nn.Linear(sv_emb)
-        score_x = hidden.dot(score_x)
+    def attend(self, a_emb, sv_emb):
+        score_x = nn.Linear(a_emb, sv_emb)    #need another agrument - would it be a_emb?
+        score_x = hidden.dot(score_x)  #what is hidden.dot?
         
  #       state_x = nn.Linear(hidden_size,attn)   
 #        score_x = nn.Tanh(nn.Linear(state_x))
@@ -192,40 +181,53 @@ class WenAttn(nn.Module):
    
     
 class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size, n_layers, dropout_p):    
+    def __init__(self, input_size, hidden_size, n_layers, dropout_p):    
         super(DecoderRNN, self).__init__()
         
         # Keep parameters for reference
+        self.input_size = input_size
         self.hidden_size = hidden_size
-        self.output_size = output_size
         self.n_layers = n_layers
         self.dropout_p = dropout_p
-        
+
+        #lstm weighted matrices
+        self.Wgate = 0.3 * torch.randn(self.hidden_size * 3, self.hidden_size * 4, dtype = torch.float64)
+        self.Who = 0.3 * torch.randn(self.hidden_size, self.input_size, dtype = torch.float64)
+
+
+        self.Wgate = torch.clamp(self.Wgate, min = -1.0, max = 1.0)
+        self.Who = torch.clamp(self.Who, min = -1.0, max = 1.0)
+
+        self.Wgate_parameter = nn.Parameter(self.Wgate)
+        self.Who_parameter = nn.Parameter(self.Who)
         # Define layers
  #       self.embedding = nn.Embedding(output_size, hidden_size)
         #change to LSTM not GRU
-        self.lstm = LSTM(hidden_size * 2, hidden_size, n_layers) #, dropout=dropout_p)
-        self.out = nn.Linear(hidden_size * 2, output_size)
+        self.lstm = WenLSTM(self.input_size, self.hidden_size, self.Wgate, self.Who) #, dropout=dropout_p)
+ #       self.out = nn.Linear(hidden_size * 2, output_size)
  #       self.softmax = nn.LogSoftmax(dim=1)
  #       self.cross_entrophy = nn.CrossEntropyLoss()
 
         self.attn = WenAttn(hidden_size)
      
     
-    def forward(self, input, last_context, a_emb, sv_emb):
+    def forward(self, a_emb, sv_emb, words_emb):
         # Note: we run this one step at a time
         
         # Get the embedding of the current input word (last output word)
    #     word_embedded = nn.Embedding(input).view(1, 1, -1) # S=1 x B x N
 
-        attn_weights = self.attn(self.hidden_size,sv_emb)
+       #input words embedding
+ #      wv_t =
+
+        attn_weights = self.attn(a_emb, sv_emb)
 
         sv_emb_t = torch.dot(attn_weights, sv_emb)
         da_emb_t = nn.Tanh(a_emb + sv_emb_t)
         # Combine embedded input word and last context, run through RNN
  #      decoder_input = torch.cat((word_embedded, last_context.unsqueeze(0)), 2)
         #change to LSTM
-        decoder_output, hidden = self.lstm(decoder_input, last_hidden)
+        decoder_output, hidden = self.lstm(wv_t, h_tm1, da_emb_t)
         
         
         # Calculate attention from current RNN state and all encoder outputs; apply to encoder outputs
